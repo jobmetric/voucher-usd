41e131f483bd5619c7efd59727eedc159dcc1a0e
majidmohammadian <majeedmohammadian@gmail.com>
2024-11-25 21:47:52 +0330

    added config file

---
+++ config/config.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Time
+    |--------------------------------------------------------------------------
+    |
+    | Cache time for get data voucher usd
+    |
+    | - set zero for remove cache
+    | - set null for forever
+    |
+    | - unit: minutes
+    */
+
+    "cache_time" => env("VOUCHER_USD_CACHE_TIME", 0),
+
+    /*
+    |--------------------------------------------------------------------------
+    | API URLS
+    |--------------------------------------------------------------------------
+    |
+    | API URLS for get data voucher usd
+    |
+    */
+
+    'api_urls' => [
+        'auth' => env('VOUCHER_USD_API_AUTH', 'https://cst-ids.voucherusd.com/api/v1'),
+        'base' => env('VOUCHER_USD_API_BASE', 'https://api.voucherusd.com/api/v1')
+    ],
+
+];
