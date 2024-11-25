fd7957714b5aa15c67389b73e56fc586ea2c210c
majidmohammadian <majeedmohammadian@gmail.com>
2024-11-25 21:47:44 +0330

    added language file

---
+++ lang/fa/base.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Voucher USD Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Voucher USD for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'package' => 'Voucher USD',
+
+];
